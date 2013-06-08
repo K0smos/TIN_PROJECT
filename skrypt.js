d53083dc56e839df2bd4911d3584b8cb89a6e0ad
Marek <kosmowski.marek@gmail.com>
2013-06-08 13:18:30 +0200

    new

--- skrypt.js
+++ skrypt.js
@@ -14,11 +14,19 @@ $(document).ready(function () {
         console.log('connected!');
     });
 
+	socket.on('zlyLogin', function(msg){
+        $('#login').append('<br><font size="2">'+msg+'</font>');
+    });
+	
+	socket.on('newTime', function(time){
+        $('#zegar').html('<br><font size="2">'+time+'</font>');
+    });
+	
+	socket.on('newTask', function(data){
 	
 	
 	
-	
-	
+	});
 	
 	
 	
