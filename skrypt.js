1d73e394d43fe25c2d608b61276adebfada91e76
Marek <kosmowski.marek@gmail.com>
2013-06-08 13:08:05 +0200

    skrypt

--- skrypt.js
+++ skrypt.js
@@ -2,5 +2,32 @@ $(document).ready(function () {
 	'use strict';
     var socket = io.connect(),
         myName = '';
+	
+	$('#addTask').hide();
+    $('#taska').hide();
+	$('#zegar').show();
+	$('#piv').hide();
+	$('#privv').hide();
+    console.log('connecting…');
+	
+	socket.on('connect', function () {
+        console.log('connected!');
+    });
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 });
